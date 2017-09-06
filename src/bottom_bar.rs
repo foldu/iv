14817c2d58b5227166a11c991efa4b239a1c624b
foldu <foldu@protonmail.com>
2017-09-06 12:57:59 +0200

    Scale with screen size

--- src/bottom_bar.rs
+++ src/bottom_bar.rs
@@ -1,5 +1,6 @@
 use gtk;
 use gtk::prelude::*;
+use pango;
 
 type Percent = f64;
 
@@ -8,17 +9,21 @@ pub struct BottomBar {
     errtext: gtk::Label,
     filename: gtk::Label,
     zoom: gtk::Label,
+    image_index: gtk::Label,
 }
 
 impl BottomBar {
     pub fn new() -> BottomBar {
         let errtext = gtk::Label::new(None);
         let filename = gtk::Label::new(None);
+        filename.set_ellipsize(pango::EllipsizeMode::End);
         let zoom = gtk::Label::new(None);
+        let image_index = gtk::Label::new(None);
         let boxx = gtk::Box::new(gtk::Orientation::Horizontal, 5);
         boxx.pack_start(&errtext, true, false, 0);
-        boxx.pack_start(&filename, true, false, 0);
-        boxx.pack_end(&zoom, true, false, 0);
+        boxx.pack_start(&filename, false, false, 0);
+        boxx.pack_start(&zoom, true, false, 0);
+        boxx.pack_end(&image_index, true, false, 0);
         boxx.set_valign(gtk::Align::End);
         boxx.set_halign(gtk::Align::End);
         BottomBar {
@@ -26,6 +31,7 @@ impl BottomBar {
             errtext: errtext,
             filename: filename,
             zoom: zoom,
+            image_index: image_index,
         }
     }
 
@@ -43,8 +49,15 @@ impl BottomBar {
 
     pub fn set_zoom(&self, percent: Option<Percent>) {
         match percent {
-            Some(percent) => self.zoom.set_text(&format!("{:.*}%", 2, percent * 100.)),
+            Some(percent) => self.zoom.set_text(&format!("| {:.*}%", 2, percent * 100.)),
             None => self.zoom.set_text(""),
+        }
+    }
+
+    pub fn set_index(&self, index: Option<(usize, usize)>) {
+        match index {
+            Some((i, n)) => self.image_index.set_text(&format!("| {}/{}", i, n)),
+            None => self.image_index.set_text(""),
         }
     }
 }
