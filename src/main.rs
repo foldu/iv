02d021d9e59196aae3981a420144ce39dcebb6a4
foldu <foldu@protonmail.com>
2017-09-08 15:01:37 +0200

    Add --recursive

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ extern crate gdk;
 extern crate gio;
 extern crate pango;
 extern crate clap;
+extern crate walkdir;
+extern crate rayon;
 
 use std::process::exit;
 use std::path::PathBuf;
 
 use clap::{App, Arg};
+use walkdir::WalkDir;
+use rayon::prelude::*;
 
 mod scrollable_image;
 mod bottom_bar;
@@ -29,16 +33,44 @@ fn main() {
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("It views images")
-        .arg(Arg::with_name("IMAGES")
+        .arg(Arg::with_name("FILES")
                  .help("The images you want to view, skips things that are not images")
                  .required(true)
                  .multiple(true))
+        .arg(Arg::with_name("recursive")
+                 .help("Traverse arguments recursively (enters directories)")
+                 .short("r")
+                 .long("recursive"))
         .get_matches();
 
-    let images: Vec<_> = opt.values_of_os("IMAGES")
-        .unwrap()
-        .map(|s| PathBuf::from(s))
-        .collect();
+    let images: Vec<_> = if opt.is_present("recursive") {
+        let mut ret = Vec::new();
+        for arg in opt.values_of_os("FILES").unwrap() {
+            let path = PathBuf::from(&arg);
+            if path.is_file() {
+                ret.push(path);
+            } else {
+                for entry in WalkDir::new(path) {
+                    match entry {
+                        Ok(ref entry) if entry.file_type().is_file() => {
+                            ret.push(PathBuf::from(entry.path()))
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        // recursive dirwalking can produce a huge amount of results so why not sort it
+        // in parallel
+        ret.par_sort_unstable();
+        ret
+    } else {
+        opt.values_of_os("FILES")
+            .unwrap()
+            .map(|s| PathBuf::from(s))
+            .collect()
+    };
 
     let app = Viewer::new(images);
 
