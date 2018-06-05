a5c989f10672e795cf517e5a3ecc0d007235554f
foldu <foldu@protonmail.com>
2018-06-05 10:55:49 +0200

    Properly use errors in zip

--- src/extract.rs
+++ src/extract.rs
@@ -8,29 +8,41 @@ use zip::{self, ZipArchive};
 #[derive(Debug, Fail)]
 pub enum ZipError {
     #[fail(display = "{}", _0)]
-    Io(io::Error),
+    Io(#[cause] io::Error),
     #[fail(display = "{}", _0)]
-    Zip(zip::result::ZipError),
+    Zip(#[cause] zip::result::ZipError),
+}
+
+impl From<io::Error> for ZipError {
+    fn from(e: io::Error) -> Self {
+        ZipError::Io(e)
+    }
+}
+
+impl From<zip::result::ZipError> for ZipError {
+    fn from(e: zip::result::ZipError) -> Self {
+        ZipError::Zip(e)
+    }
 }
 
 pub fn tmp_extract_zip<P: AsRef<Path>>(path: P) -> Result<TempDir, ZipError> {
     let path = path.as_ref();
-    let mut fh = BufReader::new(File::open(&path).map_err(ZipError::Io)?);
-    let mut zip = ZipArchive::new(&mut fh).map_err(ZipError::Zip)?;
-    let ret = TempDir::new().map_err(ZipError::Io)?;
+    let mut fh = BufReader::new(File::open(&path)?);
+    let mut zip = ZipArchive::new(&mut fh)?;
+    let ret = TempDir::new()?;
 
     for i in 0..zip.len() {
-        let mut elem = zip.by_index(i).map_err(ZipError::Zip)?;
+        let mut elem = zip.by_index(i)?;
         let out_path = ret.path().join(elem.sanitized_name());
         if let Some(parent) = out_path.parent() {
-            fs::create_dir_all(parent).map_err(ZipError::Io)?;
+            fs::create_dir_all(parent)?;
         }
 
         if elem.name().ends_with('/') {
-            fs::create_dir_all(&out_path).map_err(ZipError::Io)?;
+            fs::create_dir_all(&out_path)?;
         } else {
-            let mut out_fh = File::create(&out_path).map_err(ZipError::Io)?;
-            io::copy(&mut elem, &mut out_fh).map_err(ZipError::Io)?;
+            let mut out_fh = File::create(&out_path)?;
+            io::copy(&mut elem, &mut out_fh)?;
         }
     }
 
